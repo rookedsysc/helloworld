9202ecdbde248556422b4713941fee7fd03c995a
rookedsysc <rookedsysc36@gmail.com>
2024-12-03 18:07:53 +0900

    feat: open-api-request 프로젝트 생성

---
+++ open-api-request/src/main/java/org/rookedsysc/openapirequest/OpenApiRequestApplication.java
@@ -0,0 +1,13 @@
+package org.rookedsysc.openapirequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiRequestApplication.class, args);
+	}
+
+}
