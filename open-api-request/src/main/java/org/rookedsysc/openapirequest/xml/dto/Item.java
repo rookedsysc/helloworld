f898603d41fb9fd6dc6a1a97047578b017dae9fa
rookedsysc <rookedsysc36@gmail.com>
2024-12-04 09:07:49 +0900

    feat: Item class JacksonXmlProperty 명시

--- open-api-request/src/main/java/org/rookedsysc/openapirequest/xml/dto/Item.java
+++ open-api-request/src/main/java/org/rookedsysc/openapirequest/xml/dto/Item.java
@@ -1,12 +1,16 @@
 package org.rookedsysc.openapirequest.xml.dto;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Item {
+    @JacksonXmlProperty(localName = "sickCd")
     private String sickCd;
+    @JacksonXmlProperty(localName = "sickEngNm")
     private String sickEngNm;
+    @JacksonXmlProperty(localName = "sickNm")
     private String sickNm;
 }
