626992de792248b06ba6b4d704542453d84da31a
rookedsysc <rookedsysc36@gmail.com>
2024-12-04 16:30:20 +0900

    feat: Annotation으로 Mapping 하는 RestClient 방식 구현 및 테스트 코드 작성

---
+++ open-api-request/src/main/java/org/rookedsysc/openapirequest/json/JsonAnnotationRestClient.java
@@ -0,0 +1,43 @@
+package org.rookedsysc.openapirequest.json;
+
+import lombok.RequiredArgsConstructor;
+import org.rookedsysc.openapirequest.ApiKey;
+import org.rookedsysc.openapirequest.json.dto.SignalJsonAnnotation;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * 신호제어기 : https://t-data.seoul.go.kr/dataprovide/trafficdataviewopenapi.do?data_id=10120
+ */
+@Service
+@RequiredArgsConstructor
+public class JsonAnnotationRestClient {
+
+    private final ApiKey apiKey;
+
+    public List<SignalJsonAnnotation> getSignalJsonAnnotation() {
+        RestClient restClient = RestClient.builder()
+            .baseUrl("https://t-data.seoul.go.kr")
+            .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader("Accept-Charset", StandardCharsets.UTF_8.name())
+            .build();
+
+        String encodedApiKey = URLEncoder.encode(apiKey.signal(), StandardCharsets.UTF_8);
+
+        List<SignalJsonAnnotation> signalJsonAnnotation = restClient.get()
+            .uri("/apig/apiman-gateway/tapi/v2xSignalPhaseTimingInformation/1.0?apikey=" + apiKey.signal() + "&pageNo=1&numOfRows=10"
+            )
+            .retrieve()
+            .body(new ParameterizedTypeReference<List<SignalJsonAnnotation>>() {
+            });
+
+        return signalJsonAnnotation;
+    }
+}
---
+++ open-api-request/src/test/java/org/rookedsysc/openapirequest/json/JsonAnnotationRestClientTest.java
@@ -0,0 +1,28 @@
+package org.rookedsysc.openapirequest.json;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.rookedsysc.openapirequest.json.dto.SignalJsonAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class JsonAnnotationRestClientTest {
+
+    @Autowired
+    private JsonAnnotationRestClient jsonAnnotationRestClient;
+
+    @Test
+    @DisplayName("API 호출 정상적으로 잘 되는지 테스트")
+    void getSignalJsonAnnotationTest() {
+        // given // when
+        List<SignalJsonAnnotation> responses = jsonAnnotationRestClient.getSignalJsonAnnotation();
+
+        // then
+        assertThat(responses.size()).isEqualTo(10);
+    }
+}
