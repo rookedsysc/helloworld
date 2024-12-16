cba12c954717f5bd373f17aaa1935661f5498523
rookedsysc <rookedsysc36@gmail.com>
2024-12-16 14:35:31 +0900

    feat: project create

---
+++ oauth-oidc/src/main/java/org/rookedsysc/oauthoidc/OauthOidcApplication.java
@@ -0,0 +1,13 @@
+package org.rookedsysc.oauthoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthOidcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthOidcApplication.class, args);
+    }
+
+}
