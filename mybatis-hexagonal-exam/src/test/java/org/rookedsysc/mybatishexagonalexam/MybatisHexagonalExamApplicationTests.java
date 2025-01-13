0f63dcfe134cc6f3becbbf4af473534d60f544a0
rookedsysc <rookedsysc36@gmail.com>
2025-01-13 17:25:20 +0900

    mybatis project init

---
+++ mybatis-hexagonal-exam/src/test/java/org/rookedsysc/mybatishexagonalexam/MybatisHexagonalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rookedsysc.mybatishexagonalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisHexagonalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
