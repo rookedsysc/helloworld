41367b10fe5a712c949992f42ce0ff0a88ffa018
rookedsysc <rookedsysc36@gmail.com>
2025-03-12 15:49:39 +0900

    feat: PostUploadService에 Transactional 추가

--- mybatis-hexagonal-exam/src/main/java/org/rookedsysc/mybatishexagonalexam/post/application/PostUploadService.java
+++ mybatis-hexagonal-exam/src/main/java/org/rookedsysc/mybatishexagonalexam/post/application/PostUploadService.java
@@ -8,8 +8,10 @@ import org.rookedsysc.mybatishexagonalexam.post.application.port.in.PostUploadUs
 import org.rookedsysc.mybatishexagonalexam.post.application.port.out.PostUploadPort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PostUploadService implements PostUploadUsecase {
 
