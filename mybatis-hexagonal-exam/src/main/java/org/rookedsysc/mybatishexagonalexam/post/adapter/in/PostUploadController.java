ec164f7e7ab214fb6d016c8afae5721de3f47133
rookedsysc <rookedsysc36@gmail.com>
2025-03-12 14:00:06 +0900

    feat: PostUploadController 구현

---
+++ mybatis-hexagonal-exam/src/main/java/org/rookedsysc/mybatishexagonalexam/post/adapter/in/PostUploadController.java
@@ -0,0 +1,21 @@
+package org.rookedsysc.mybatishexagonalexam.post.adapter.in;
+
+import lombok.RequiredArgsConstructor;
+import org.rookedsysc.mybatishexagonalexam.post.application.port.in.PostCommand;
+import org.rookedsysc.mybatishexagonalexam.post.application.port.in.PostUploadUsecase;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
+public class PostUploadController {
+
+    private final PostUploadUsecase postUploadUsecase;
+
+    @PostMapping
+    public long upload(PostCommand command) {
+        return postUploadUsecase.upload(command);
+    }
+}
