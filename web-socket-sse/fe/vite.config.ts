ad1638f630c4839d8150782e53a9628053258bc3
rookedsysc <rookedsysc36@gmail.com>
2025-04-16 22:45:50 +0900

    fix: Socket-JS Client가 브라우저 환경에서 Node.js의 global 객체를 못찾는 에러 수정
    Uncaught ReferenceError: global is not defined
        at node_modules/sockjs-client/lib/utils/browser-crypto.js
    (sockjs-client.js?v=01b90a21:9:5)
        at __require (chunk-3O7X656O.js?v=01b90a21:11:50)
        at node_modules/sockjs-client/lib/utils/random.js
    (sockjs-client.js?v=01b90a21:31:18)
        at __require (chunk-3O7X656O.js?v=01b90a21:11:50)
        at node_modules/sockjs-client/lib/utils/event.js
    (sockjs-client.js?v=01b90a21:59:18)
        at __require (chunk-3O7X656O.js?v=01b90a21:11:50)
        at node_modules/sockjs-client/lib/transport/websocket.js
    (sockjs-client.js?v=01b90a21:1062:17)
        at __require (chunk-3O7X656O.js?v=01b90a21:11:50)
        at node_modules/sockjs-client/lib/transport-list.js
    (sockjs-client.js?v=01b90a21:2631:7)
        at __require (chunk-3O7X656O.js?v=01b90a21:11:50)

--- web-socket-sse/fe/vite.config.ts
+++ web-socket-sse/fe/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    global: "window",
+    "process.env": {},
+  },
+  resolve: {
+    alias: {
+      // This is needed for SockJS to work properly in browser
+      stream: "stream-browserify",
+      buffer: "buffer",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser global
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+});
