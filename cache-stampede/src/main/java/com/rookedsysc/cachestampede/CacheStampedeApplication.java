e36237cd8d5d2800eda9551adb04baabdfcaeffb
rookedsysc <rookedsysc36@gmail.com>
2025-08-30 21:47:24 +0900

    init

---
+++ cache-stampede/src/main/java/com/rookedsysc/cachestampede/CacheStampedeApplication.java
@@ -0,0 +1,13 @@
+package com.rookedsysc.cachestampede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStampedeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheStampedeApplication.class, args);
+    }
+
+}
---
+++ cache-stampede/src/test/java/com/rookedsysc/cachestampede/CacheStampedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookedsysc.cachestampede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStampedeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
