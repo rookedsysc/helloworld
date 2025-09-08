9f3f9b0523cf6460bc7cf4c0e42eadd31c13ac18
rookedsysc <rookedsysc36@gmail.com>
2025-09-08 22:45:33 +0900

    init spring boot

---
+++ src/test/java/com/rookedsysc/distributedtransaction/DistributedTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookedsysc.distributedtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
