ad100d186f6d17f4cf24fe1a6072b78de0e41744
rookedsysc <rookedsysc36@gmail.com>
2025-09-30 19:02:54 +0900

    build: 멀티 모듈 설정

--- distributed-transaction/build.gradle.kts
+++ distributed-transaction/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.rookedsysc"
@@ -19,20 +16,10 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+subprojects {
+    apply(plugin = "kotlin")
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    repositories {
+        mavenCentral()
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
--- distributed-transaction/settings.gradle.kts
+++ distributed-transaction/settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "distributed-transaction"
+include("monolithic")
