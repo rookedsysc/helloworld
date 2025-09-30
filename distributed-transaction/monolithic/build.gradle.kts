44a598bdc3d76d2346a70ee37b23629802843a40
rookedsysc <rookedsysc36@gmail.com>
2025-09-30 20:23:51 +0900

    config: multi module dependency 공통화

--- distributed-transaction/monolithic/build.gradle.kts
+++ distributed-transaction/monolithic/build.gradle.kts
@@ -1,34 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
-    id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "1.9.25"
 }
 
-group = "com.rookedsysc"
-version = "0.0.1-SNAPSHOT"
-description = "monolithic"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
-}
-
-repositories {
-    mavenCentral()
-}
+tasks.register("prepareKotlinBuildScriptModel") {}
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
@@ -36,26 +12,8 @@ dependencies {
     // redisson : https://mvnrepository.com/artifact/org.redisson/redisson
     implementation("org.redisson:redisson-spring-boot-starter:3.51.0")
 
-    // aop
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-
     // prometheus
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
