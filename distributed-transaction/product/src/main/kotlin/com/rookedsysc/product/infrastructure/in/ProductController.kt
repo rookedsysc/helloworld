b2d7d1005c453b465f4ceb108da21bdfed575dd1
rookedsysc <rookedsysc36@gmail.com>
2025-10-19 16:52:23 +0900

    refactor: product reservation controlelr

--- distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductController.kt
+++ distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductController.kt
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/products")
 class ProductController(
-    private val productService: ProductService,
     private val productListService: ProductListService,
     private val productCreateService: ProductCreateService
 ) {
@@ -31,7 +30,7 @@ class ProductController(
     fun list(
         @RequestParam(defaultValue = "0") page: Int,
         @RequestParam(defaultValue = "10") size: Int,
-        ): PageResponse<ProductListResponse> {
+    ): PageResponse<ProductListResponse> {
         val pageable: Pageable = Pageable.ofSize(size).withPage(page)
         val productResults: Page<ProductListResult> = productListService.list(pageable)
         val productResponses: Page<ProductListResponse> = productResults.map { productResult ->
@@ -44,13 +43,5 @@ class ProductController(
     fun create(@RequestBody request: ProductCreateRequest): ProductListResponse {
         val productResult = productCreateService.create(request.toCommand())
         return ProductListResponse.from(productResult)
-    }
-
-
-    @PostMapping("/reserve")
-    fun reserve(@RequestBody request: ProductReserveRequest): ProductReserveResponse {
-        val command = request.toCommand()
-        val totalPrice = productService.tryReserve(command).totalPrice
-        return ProductReserveResponse(totalPrice = totalPrice)
     }
 }
---
+++ distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductReservationController.kt
@@ -0,0 +1,22 @@
+package com.rookedsysc.product.infrastructure.`in`
+
+import com.rookedsysc.product.application.ProductService
+import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveRequest
+import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/products")
+class ProductReservationController(
+    private val productService: ProductService,
+) {
+    @PostMapping("/reserve")
+    fun reserve(@RequestBody request: ProductReserveRequest): ProductReserveResponse {
+        val command = request.toCommand()
+        val totalPrice = productService.tryReserve(command).totalPrice
+        return ProductReserveResponse(totalPrice = totalPrice)
+    }
+}
