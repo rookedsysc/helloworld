3e07af3a6cab68208e254be458427f017cc4280c
rookedsysc <rookedsysc36@gmail.com>
2025-10-19 17:03:44 +0900

    docs: 상품 API Swagger 구체화

--- distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductController.kt
+++ distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductController.kt
@@ -9,6 +9,7 @@ import com.rookedsysc.product.infrastructure.`in`.dto.ProductCreateRequest
 import com.rookedsysc.product.infrastructure.`in`.dto.ProductListResponse
 import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveRequest
 import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveResponse
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -20,12 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "상품 관리")
 @RestController
 @RequestMapping("/products")
 class ProductController(
     private val productListService: ProductListService,
     private val productCreateService: ProductCreateService
 ) {
+    @Operation(summary = "상품 목록 조회")
     @GetMapping
     fun list(
         @RequestParam(defaultValue = "0") page: Int,
@@ -39,6 +42,7 @@ class ProductController(
         return PageResponse.from(productResponses)
     }
 
+    @Operation(summary = "상품 생성")
     @PostMapping
     fun create(@RequestBody request: ProductCreateRequest): ProductListResponse {
         val productResult = productCreateService.create(request.toCommand())
--- distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductReservationController.kt
+++ distributed-transaction/product/src/main/kotlin/com/rookedsysc/product/infrastructure/in/ProductReservationController.kt
@@ -3,16 +3,20 @@ package com.rookedsysc.product.infrastructure.`in`
 import com.rookedsysc.product.application.ProductService
 import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveRequest
 import com.rookedsysc.product.infrastructure.`in`.dto.ProductReserveResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "상품 예약")
 @RestController
 @RequestMapping("/products")
 class ProductReservationController(
     private val productService: ProductService,
 ) {
+    @Operation(summary = "상품 예약 생성")
     @PostMapping("/reserve")
     fun reserve(@RequestBody request: ProductReserveRequest): ProductReserveResponse {
         val command = request.toCommand()
