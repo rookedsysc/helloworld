4d5dd1d97f5d1c3a14a5abd926d12c2e5a0d90e4
rookedsysc <rookedsysc36@gmail.com>
2025-11-16 02:29:45 +0900

    feat: multi file prisma 설정 및 test model 추가

--- nest-js-poc/prisma.config.ts
+++ nest-js-poc/prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, env } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'prisma/schema',
   migrations: {
     path: 'prisma/migrations',
   },
