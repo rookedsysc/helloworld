655ab5c9e4172bfca7c639543207df404b7d71a9
rookedsysc <rookedsysc36@gmail.com>
2025-11-16 03:20:00 +0900

    feat: swagger dependency 추가 및 swagger 설정

--- nest-js-poc/src/main.ts
+++ nest-js-poc/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Setup //
+  const config = new DocumentBuilder()
+    .setTitle('NestJS POC')
+    .setDescription('입사 전 Nest POC 프로젝트 API 문서')
+    .setVersion('v1')
+    .build();
+  const documents = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, documents, {
+    swaggerOptions: {
+      filter: true,
+      displayRequestDuration: true,
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
