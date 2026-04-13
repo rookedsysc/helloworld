0c0838390dedbaea0c03dc0a82b36251f6313d86
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2026-04-13 21:18:01 +0900

    otel project init (#73)

---
+++ otel-example-spring-java/src/test/kotlin/com/roky/otelexamplespringjava/OtelExampleSpringJavaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roky.otelexamplespringjava
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OtelExampleSpringJavaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
